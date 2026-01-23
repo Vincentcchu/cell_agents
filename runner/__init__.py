b9696f53cee217d1634a1906d427d77a09ca2b3e
Vincent CHU <shekchu@canada-l.cs.ucl.ac.uk>
2026-01-23 12:12:01 +0000

    remove scExtract submodule

--- runner/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Unified interface for running agent sub-repositories."""
-from .agent_runner import AgentRunner
-from .base_agent import AgentRequest, AgentResult, BaseAgent
-
-__all__ = ["AgentRunner", "AgentRequest", "AgentResult", "BaseAgent"]
--- runner/agents/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Import agent wrappers so they register themselves via decorators."""
-
-from . import biomni_agent  # noqa: F401
-from . import biomaster_agent  # noqa: F401
-from . import cassia_agent  # noqa: F401
-from . import celltypeagent_agent  # noqa: F401
-from . import mllm_agent  # noqa: F401
